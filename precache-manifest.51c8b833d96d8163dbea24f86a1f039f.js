fd1efb0749837ac23d3eb8e1b9619e8c4ae8df5a
Roma <roman.mykhalchuk@yahoo.com>
2020-09-27 16:57:00 +0300

    Updates

---
+++ precache-manifest.51c8b833d96d8163dbea24f86a1f039f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8825cad7763c53047ddc259375b86f86",
+    "url": "/whatsapp-clone/index.html"
+  },
+  {
+    "revision": "5b385ff101527690449e",
+    "url": "/whatsapp-clone/static/css/main.508a50ff.chunk.css"
+  },
+  {
+    "revision": "75831c811a9f7e62a433",
+    "url": "/whatsapp-clone/static/js/2.a43194bc.chunk.js"
+  },
+  {
+    "revision": "95e5393dbc94b774bee63119a26602ef",
+    "url": "/whatsapp-clone/static/js/2.a43194bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b385ff101527690449e",
+    "url": "/whatsapp-clone/static/js/main.983af1b4.chunk.js"
+  },
+  {
+    "revision": "6a8fb725ea9406a0da76",
+    "url": "/whatsapp-clone/static/js/runtime-main.eeb36d07.js"
+  }
+]);
