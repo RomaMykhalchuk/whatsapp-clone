a63effff78a6cf1d6cb0c2fa6bc03b40fc1909c6
Roma <roman.mykhalchuk@yahoo.com>
2020-09-27 17:28:54 +0300

    fixed issued in backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://wapp-clone.herokuapp.com",
+  baseURL: "https://whatsapp-clone1.herokuapp.com",
 });
 
 export default instance;
